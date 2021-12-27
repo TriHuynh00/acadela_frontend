a64d6e8512f4d9fd7bd39cbcecb8874fabbfc50e
Tri Huynh <tri.huynh@tum.de>
2021-12-27 14:17:56 +0100

    Add auto complete for elements in Acadela

--- src/acadelaSetting/NonTerminalSymbol.js
+++ src/acadelaSetting/NonTerminalSymbol.js
@@ -2,7 +2,7 @@
 export default class NonTerminalSymbol {
     static OBJECT = 'obj-keywords';
     static ATTRIBUTE = 'attr-keywords';
-    static STATE = 'state-keywords';
+    static DIRECTIVE = 'state-keywords';
     static ERROR = 'error';
     static STRING = 'str';
     static COMMENT = 'comment';
--- src/acadelaSetting/ThemeDefinition.js
+++ src/acadelaSetting/ThemeDefinition.js
@@ -6,7 +6,7 @@ export default class ThemeDefinition {
         rules: [
             { token: NonTerminalSymbol.OBJECT, foreground: '660066' /*purple*/, fontStyle: 'bold' },
             { token: NonTerminalSymbol.ATTRIBUTE, foreground: '1167b1' /*dark blue*/, fontStyle: 'bold' },
-            { token: NonTerminalSymbol.STATE, foreground: 'E08400' /*dark blue*/, fontStyle: 'bold' },
+            { token: NonTerminalSymbol.DIRECTIVE, foreground: 'E08400' /*dark blue*/, fontStyle: 'bold' },
             { token: NonTerminalSymbol.ERROR, foreground: 'b4151c' /*red*/ },
             { token: NonTerminalSymbol.STRING, foreground: '4ca973' /*green*/, fontStyle: 'bold' },
             { token: NonTerminalSymbol.COMMENT, foreground: 'bebebe' /*grey*/ },
